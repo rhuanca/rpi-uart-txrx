38f4942e692c9f77162eeedd9f9389f30d472a37
Renan Huanca <rhuanca@gmail.com>
2015-08-02 12:27:36 -0400

    added rx and tx

--- rpi_uart_rx.c
+++ rpi_uart_rx.c
@@ -6,6 +6,8 @@
 int main() {
     int uart0_filestream = -1;
 
+    printf("UART RX\n");
+
     // Setup the uart
     // ---
     uart0_filestream = open("/dev/ttyAMA0", O_RDWR | O_NOCTTY | O_NDELAY);		//Open in non blocking read/write mode
@@ -26,26 +28,30 @@ int main() {
     tcflush(uart0_filestream, TCIFLUSH);
     tcsetattr(uart0_filestream, TCSANOW, &options);
 
-    // Transmiting bytes
-    // ---
-
-    unsigned char tx_buffer[20];
-    unsigned char *p_tx_buffer;
-
-    p_tx_buffer = &tx_buffer[0];
-    *p_tx_buffer++ = 'H';
-    *p_tx_buffer++ = 'e';
-    *p_tx_buffer++ = 'l';
-    *p_tx_buffer++ = 'l';
-    *p_tx_buffer++ = 'o';
-
-    // Sending data
-    // ---
-    int count = write(uart0_filestream, &tx_buffer[0], (p_tx_buffer - &tx_buffer[0]));		//Filestream, bytes to write, number of bytes to write
-    if (count < 0)
-    {
-        printf("UART TX error\n");
+    printf("Receiving data\n");
+    while(1) {
+        // Receiving data
+        // ---
+        // Read up to 255 characters from the port if they are there
+        unsigned char rx_buffer[256];
+        int rx_length = read(uart0_filestream, (void*)rx_buffer, 255);		//Filestream, buffer to store in, number of bytes to read (max)
+        if (rx_length < 0)
+        {
+            //An error occured (will occur if there are no bytes)
+        }
+        else if (rx_length == 0)
+        {
+            //No data waiting
+        }
+        else
+        {
+            //Bytes received
+            rx_buffer[rx_length] = '\0';
+            printf("%i bytes read : %s\n", rx_length, rx_buffer);
+        }
     }
+
+
 
     close(uart0_filestream);
     printf("serial port closed.\n");
---
+++ rpi_uart_tx.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <unistd.h>			//Used for UART
+#include <fcntl.h>			//Used for UART
+#include <termios.h>		//Used for UART
+
+int main() {
+    int uart0_filestream = -1;
+
+    printf("UART TX\n");
+    // Setup the uart
+    // ---
+    uart0_filestream = open("/dev/ttyAMA0", O_RDWR | O_NOCTTY | O_NDELAY);		//Open in non blocking read/write mode
+    if (uart0_filestream == -1)
+    {
+        //ERROR - CAN'T OPEN SERIAL PORT
+        printf("Error - Unable to open UART.  Ensure it is not in use by another application\n");
+        return 0;
+    }
+    printf("serial port opened.\n");
+
+    struct termios options;
+    tcgetattr(uart0_filestream, &options);
+    options.c_cflag = B9600 | CS8 | CLOCAL | CREAD;		//<Set baud rate
+    options.c_iflag = IGNPAR;
+    options.c_oflag = 0;
+    options.c_lflag = 0;
+    tcflush(uart0_filestream, TCIFLUSH);
+    tcsetattr(uart0_filestream, TCSANOW, &options);
+
+    // Transmiting bytes
+    // ---
+
+    unsigned char tx_buffer[20];
+    unsigned char *p_tx_buffer;
+
+    p_tx_buffer = &tx_buffer[0];
+    *p_tx_buffer++ = 'H';
+    *p_tx_buffer++ = 'e';
+    *p_tx_buffer++ = 'l';
+    *p_tx_buffer++ = 'l';
+    *p_tx_buffer++ = 'o';
+
+    // Sending data
+    // ---
+    int count = write(uart0_filestream, &tx_buffer[0], (p_tx_buffer - &tx_buffer[0]));		//Filestream, bytes to write, number of bytes to write
+    if (count < 0)
+    {
+        printf("UART TX error\n");
+    }
+
+    close(uart0_filestream);
+    printf("serial port closed.\n");
+    return 0;
+}
